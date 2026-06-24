71d05e3ded87e9ecd5e22deb12f85a51fd1cac37
Sanya9700 <sanya25051991@gmail.com>
2026-06-24 09:30:37 +0400

    add btn and number

--- history.js
+++ history.js
@@ -3,8 +3,10 @@ const result = document.querySelector(".result");
 const searchFilms = JSON.parse(localStorage.getItem('searchFilm'));
 for(let searchFilm of searchFilms){
     const searchHtml = `
-      <div class="search__film">
+        <li>     
+            <div class="search__film">
   <p class="search__film-title">Название фильма: ${searchFilm.name}</p>
+</li>
     `;
     result.insertAdjacentHTML('afterbegin', searchHtml)
 }
--- scriptFavorite.js
+++ scriptFavorite.js
@@ -3,10 +3,12 @@ const result = document.querySelector(".result");
 const favoritFilms = JSON.parse(localStorage.getItem("favorites"));
 for (let favoritFilm of favoritFilms) {
   const filmHtml = `
- <div class="favorite__film">
+  <li>
   <p class="favorite__film-title">Название фильма: ${favoritFilm.name}</p>
   <p class="favorite__film-year">Год производста: ${favoritFilm.year}</p>
+  </li>
     `;
   result.insertAdjacentHTML('afterbegin', filmHtml)
   console.log(favoritFilm);
 }
+
